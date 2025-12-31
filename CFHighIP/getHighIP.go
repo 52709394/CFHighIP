5387c0f349f5c5de63bf53669c6cfde0f069ef5c
52709394 <186333925+52709394@users.noreply.github.com>
2025-12-31 14:19:01 +0800

    Update getHighIP.go

--- CFHighIP/getHighIP.go
+++ CFHighIP/getHighIP.go
@@ -10,17 +10,22 @@ func main() {
 
 `
 
-	pattern := `(\d+\.\d+\.\d+\.\d+)\s*(\d+)\s*https\s*403 Forbidden`
-
-	re := regexp.MustCompile(pattern)
+	re := regexp.MustCompile(`(?m)^\s*https\:\/\/(\d+\.\d+\.\d+\.\d+(?:\:\d+|))\s+`)
 
 	matches := re.FindAllStringSubmatch(text, -1)
 
 	for _, m := range matches {
-		ip := m[1] + ":" + m[2]
+		var ip string
+
+		if matched, _ := regexp.MatchString(`\:\d+$`, m[1]); matched {
+			ip = m[1]
+		} else {
+			ip = m[1] + ":443"
+		}
 
 		fmt.Println(``)
 
 	}
 }
+
 
