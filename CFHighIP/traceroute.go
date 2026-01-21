00cd9d210b6ec43516e9efa04d775ca70f87ad3f
52709394 <186333925+52709394@users.noreply.github.com>
2026-01-21 17:00:09 +0800

    Add files via upload

---
+++ CFHighIP/traceroute.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	text := `
+
+	`
+
+	pattern := `(\d+\.\d+\.\d+\.\d+)(\/\d+)`
+
+	re := regexp.MustCompile(pattern)
+
+	matches := re.FindAllStringSubmatch(text, -1)
+
+	for _, ip := range matches {
+
+		out, err := exec.Command(
+			"./nexttrace_windows_amd64.exe",
+			"--source",
+			"192.168.3.83",
+			ip[1]).CombinedOutput()
+
+		if err != nil {
+			fmt.Println("err:", err)
+			continue
+		}
+
+		if strings.Contains(string(out), "[CN2-BackBone]") {
+			fmt.Println(ip[1] + ip[2])
+		} else {
+			fmt.Println(ip[1] + ip[2] + "不是")
+		}
+
+	}
+
+}
